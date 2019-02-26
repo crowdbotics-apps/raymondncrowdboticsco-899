8bcc21738baed64813d98950859f5be8d0074e77
Raymond <raymond.n@crowdbotics.com>
2019-02-26 12:13:47 +0400

    [WIP] Campaign Details Screen Implementation

--- src/routes/ContestDetails/index.js
+++ src/routes/ContestDetails/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import FastImage from 'react-native-fast-image';
+import Video from 'react-native-video';
+import moment from 'moment';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { AppContext, Navbar, Button } from 'app/components';
 import { CampaignController } from 'app/controllers';
@@ -15,15 +18,20 @@ class ContestDetailsScreen extends React.Component {
     super(props);
 
     this.state = {
-      data: null
+      data: null,
+      mediaStates: []
     };
+
+    this.players = {};
   }
 
   async componentDidMount() {
     let campaignId = this.props.navigation.state.params.campaignId;
     let data = await CampaignController.getCampaignById(campaignId);
+    let mediaStates = Array(data.questions.length).fill(true);
     this.setState({
-      data
+      data,
+      mediaStates
     });
   }
 
@@ -33,7 +41,54 @@ class ContestDetailsScreen extends React.Component {
 
   rightHandler = () => {};
 
+  playVideo = index => () => {
+    let { mediaStates } = this.state;
+    mediaStates[index] = false;
+    this.setState({ mediaStates });
+    this.players[`${index}`].seek(0);
+    this.players[`${index}`].presentFullscreenPlayer();
+  };
+
+  stopVideo = index => () => {
+    let { mediaStates } = this.state;
+    mediaStates[index] = true;
+    this.setState({ mediaStates });
+  };
+
+  renderQuestion = (question, index) => {
+    return (
+      <View key={`${index}`} style={styles.questionItem}>
+        <Text style={styles.qLabel}>Question{index + 1}</Text>
+        {question.media_type &&
+          (question.media_type.includes('image/') ? (
+            <FastImage
+              source={{ uri: question.media }}
+              style={styles.qImage}
+              resizeMode={FastImage.resizeMode.contain}
+            />
+          ) : (
+            <View style={styles.qVideoContainer}>
+              <Video
+                ref={ref => (this.players[`${index}`] = ref)}
+                source={{ uri: question.media }}
+                style={styles.qVideo}
+                paused={this.state.mediaStates[index]}
+                onFullscreenPlayerDidDismiss={this.stopVideo(index)}
+              />
+              <View style={styles.qVideoOverlay}>
+                <TouchableOpacity onPress={this.playVideo(index)}>
+                  <Ionicons name="md-arrow-dropright-circle" style={styles.qVideoPlayBtn} />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
+      </View>
+    );
+  };
+
   render() {
+    let { data } = this.state;
+
     return (
       <View style={styles.container}>
         <Navbar
@@ -43,7 +98,31 @@ class ContestDetailsScreen extends React.Component {
           right="Update"
           rightHandler={this.rightHandler}
         />
-        <View style={styles.content} />
+        {data && (
+          <ScrollView style={styles.scrollContent}>
+            <View style={styles.content}>
+              <Text style={styles.title}>{data.name}</Text>
+              <FastImage
+                style={styles.logo}
+                source={data.logo ? { uri: data.logo } : NoLogoImage}
+                resizeMode={FastImage.resizeMode.contain}
+              />
+              <View style={styles.dateContainer}>
+                <View style={styles.from}>
+                  <Text style={styles.label}>Start</Text>
+                  <Text style={styles.date}>{moment(data.from).format('YYYY-MM-DD')}</Text>
+                </View>
+                <View style={styles.to}>
+                  <Text style={styles.label}>End</Text>
+                  <Text style={styles.date}>{moment(data.to).format('YYYY-MM-DD')}</Text>
+                </View>
+              </View>
+              <View style={styles.qContainer}>
+                {data.questions.map((question, index) => this.renderQuestion(question, index))}
+              </View>
+            </View>
+          </ScrollView>
+        )}
       </View>
     );
   }
--- src/routes/ContestDetails/style.js
+++ src/routes/ContestDetails/style.js
@@ -5,5 +5,85 @@ let dm = Dimensions.get('screen');
 export default StyleSheet.create({
   container: {
     flex: 1
+  },
+  scrollContent: {
+    flex: 1
+  },
+  content: {
+    padding: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    alignSelf: 'center'
+  },
+  logo: {
+    width: dm.width - 40,
+    height: 200,
+    alignSelf: 'center',
+    marginVertical: 10
+  },
+  dateContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20
+  },
+  from: {
+    flex: 1,
+    marginLeft: 10,
+    alignItems: 'center'
+  },
+  to: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  label: {
+    fontSize: 14,
+    color: '#555'
+  },
+  date: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginTop: 5
+  },
+  qContainer: {
+    flex: 1
+  },
+  questionItem: {
+    marginBottom: 20
+  },
+  qLabel: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#555'
+  },
+  qImage: {
+    width: dm.width - 40,
+    height: 200,
+    marginTop: 10
+  },
+  qVideoContainer: {
+    width: dm.width - 40,
+    height: 200,
+    marginTop: 10
+  },
+  qVideo: {
+    width: dm.width - 40,
+    height: 200
+  },
+  qVideoOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  qVideoPlayBtn: {
+    fontSize: 50,
+    color: '#fff'
   }
 });
